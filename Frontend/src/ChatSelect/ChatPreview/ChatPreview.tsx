34a96a74124348af034b7748dc3d8cf88421aead
DBartmann <dbartmann16@gmail.com>
2026-09-12 11:58:26 +0100

    cut message body in preview to stop overflow

--- Frontend/src/ChatSelect/ChatPreview/ChatPreview.tsx
+++ Frontend/src/ChatSelect/ChatPreview/ChatPreview.tsx
@@ -1,6 +1,6 @@
 import  { type Chat } from "@shared/types"
 import styles from "../ChatSelect.module.css"
-import { use } from "react"
+import { use, useEffect, useState } from "react"
 import { SocketContext } from "@/Contexts/SocketContext"
 
 type ChatPreviewProps = {
@@ -10,11 +10,21 @@ type ChatPreviewProps = {
 
 const ChatPreview = ({data, index}:ChatPreviewProps) => {
     const {setSelectedChat} = use(SocketContext)
+    const [messagePreview, setMessagePreview] = useState<string>("")
+
+    useEffect(() => {
+        let newestMsg = "Start Chatting!"
+
+        if(data.messages.length > 0) newestMsg = data.messages[data.messages.length - 1].body
+        setMessagePreview(() => {
+            return newestMsg.length < 20 ? newestMsg : newestMsg.slice(0, 20) + "..." 
+        })
+    }, [data])
 
     return(
         <div className={styles.previewContainer} onClick={() => setSelectedChat(index)}>
             <h2>{data.partner}</h2>
-            <p>{data.messages[data.messages.length-1]?.body || "Start Chatting!"}</p>
+            <p>{messagePreview}</p>
         </div>
     )
 }
